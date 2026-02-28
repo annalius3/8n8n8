1f2112e13cadbf0c32dcac79c452e54bae702da8
Владислав Кот <vladkatintam@gmail.com>
2026-02-28 22:36:20 +0200

    Relax LinkButton typing for dynamic routes

--- components/ui/link-button.tsx
+++ components/ui/link-button.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
-import { type ComponentProps } from "react";
+import Link, { type LinkProps } from "next/link";
+import { type AnchorHTMLAttributes } from "react";
 import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-type LinkButtonProps = Omit<ComponentProps<typeof Link>, "className"> &
-  { className?: string } &
+type LinkButtonProps = {
+  href: LinkProps["href"];
+  className?: string;
+} & Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "className" | "href"> &
   VariantProps<typeof buttonVariants>;
 
 export function LinkButton({ className, variant, size, ...props }: LinkButtonProps) {
